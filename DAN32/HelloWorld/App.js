3a56bfc4c683457fd9e975fecc3b943f915d7397
Sjany <stienda25@alumnos.uaq.mx>
2025-08-13 13:59:02 -0600

    Update App.js

--- DAN32/HelloWorld/App.js
+++ DAN32/HelloWorld/App.js
@@ -10,7 +10,7 @@ export default function App() {
       />
       <Text style={styles.text}>
         Mi nombre es Sjany Tienda Avila, tengo 21 años, soy del estado de Hidalgo, actualmente estudio en las mañanas y trabajo en las tardes,
-        me gusta jugar LOL y flamear personas.
+        me gusta jugar LOL y flamear personas
       </Text>
     </View>
   );
